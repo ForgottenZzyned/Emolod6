e851af80f6d2a8e886dfab07e4fcb31dbe752137
FORGOTTEN <denys.yaroha@gmail.com>
2025-03-12 14:48:07 +0200

    Add files via upload

---
+++ FirstTask.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emolod6
+{
+    enum Status
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+    enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+    abstract class OrderItem
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public abstract void Prepare();
+    }
+    class Pizza : OrderItem
+    {
+        public Size size { get; set; }
+        public Pizza(double price, Size size)
+        {
+            this.price = price;
+            this.size = size;
+        }
+        public override void Prepare()
+        {
+            Console.WriteLine($"Baking {size} pizza");
+        }
+    }
+    class Drink : OrderItem
+    {
+        public bool isCold;        
+        public Drink(double price,bool isCold)
+        {
+            this.price = price;
+            this.isCold = isCold;
+        }
+
+        public override void Prepare()
+        {
+            Console.WriteLine("Pouring drink");
+        }
+    }
+
+    class Dessert : OrderItem
+    {
+        public Dessert(double price)
+        {
+            this.price= price;
+        }
+        public override void Prepare()
+        {
+            Console.WriteLine("Making dessert");
+        }
+    }
+
+    class Order
+    {
+        private List<OrderItem> items = new List<OrderItem>();
+        private Status status {  get; set; }
+        public void AddItem( OrderItem item )
+        {
+            if( item != null)
+            {
+                items.Add(item);
+                Console.WriteLine("Added item");
+            }   
+            else
+            {
+                throw new Exception("Item is Null");
+            }
+        }
+        public void ProcessOrder()
+        {
+            double totalPrice = 0;
+            if(items.Count > 0)
+            {
+                foreach (var item in items)
+                {
+                    totalPrice += item.price;
+                    item.Prepare();
+                }
+                Console.WriteLine($"Total price of your order - {totalPrice}");
+            }
+            else
+            {
+                Console.WriteLine("There is no orders to process");
+            }            
+        }
+    }
+
+    class GetUserInput
+    {
+        public int Int(string message)
+        {
+            Console.WriteLine(message);
+            int input = Convert.ToInt32(Console.ReadLine());
+            return input;
+        }
+        public string String(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+
+    internal class FirstTask
+    {
+        static GetUserInput getInput = new GetUserInput();                   
+        static void Main()
+        {
+            Order orders = new Order();
+            int i = 0;
+            while(i == 0)
+            {
+                Console.WriteLine("\n 1 - Add item to your order \n 2 - Process your order");
+                int input = getInput.Int("What do you want to do?");
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Processing add...");
+                        CreateSampleOrder(orders);
+                        break;
+                    case 2:
+                        Console.WriteLine("Processing order...");
+                        orders.ProcessOrder();
+                        i++;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect index of operation");
+                        break;
+                }
+            }
+            
+        }
+        static void CreateSampleOrder(Order orders)
+        {
+            double totalPrice = 0.00;
+            int input = getInput.Int("What kind of product you want to add to your order? \n 1 - pizza\n 2 - drink \n 3 - dessert");
+            switch(input)
+            {
+                case 1:
+                    totalPrice += 9;
+                    Size size = new Size();
+                    input = getInput.Int("What size you need? \n 1 - small, will cost 9\n 2 - medium, will cost 14\n 3 - large, will cost 16");
+                    switch (input)
+                    {
+                        case 1:
+                            totalPrice += 0;
+                            size = Size.Small;
+                            break;
+                        case 2:
+                            totalPrice += 5;
+                            size = Size.Medium;
+                            break;
+                        case 3:
+                            totalPrice += 7;
+                            size = Size.Large;
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect index \n Returning to main page");
+                            return;
+                    }
+                    
+                    OrderItem item = new Pizza(totalPrice, size);
+                    orders.AddItem(item);
+                    break;
+                case 2:
+                    totalPrice += 5;
+                    input = getInput.Int("Do you want it cold? - Will cost 5\n 1 - yes \n 2 - no");
+                    switch (input)
+                    {
+                        case 1:
+                            item = new Drink(totalPrice,true);
+                            break;
+                        case 2:
+                            item = new Drink(totalPrice,false);
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect index \n Returning to main page");
+                            return;
+
+                    }      
+                    orders.AddItem(item);
+                    break;
+                case 3:
+                    totalPrice += 7;
+                    Console.WriteLine("Dessert will cost 7");
+                    orders.AddItem(new Dessert(totalPrice));
+                    break;
+                default:
+                    Console.WriteLine("Incorrect index of product");
+                    break;
+            }
+        }
+    }
+}
---
+++ SecondTask.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emolod6._1
+{
+    enum Names
+    {
+        Ivan = 0,
+        Vasulii = 1,
+        Denys = 2,
+        Artem = 3,
+        Nikita = 4,
+        Andrew = 5,
+        Vlad = 6,
+        Stas = 7,
+        Bogdan = 8,
+        Alex = 9
+    }
+    abstract class Person
+    {
+        public int hours { get; set; }
+        public Names name { get; set; }
+        public double money { get; set; }
+        protected int salaryPerHour { get; set; } = 30;
+        protected int defaulHourCount { get; set; } = 160;
+        public uint id { get; set; }
+        public abstract void CalculateSalary(int hours);
+
+    }
+
+    class Professor : Person
+    {
+        public Professor(Names name)
+        {
+            this.id = 0;
+            this.name = name;
+        }
+        
+        public float coeff { get; set; } = 3f;
+        public override void CalculateSalary(int hours)
+        {
+            float salary = 0;
+            while(hours > defaulHourCount)
+            {
+                hours--;
+                salary += salaryPerHour * coeff * 1;
+            }
+            while(hours > 0)
+            {
+                hours--;
+                salary += salaryPerHour * 5 * 1;
+            }
+            this.money += Convert.ToDouble(salary) + 2000;
+            Console.WriteLine($"This is {this.name}, he is Professor and his salary is {salary}, his hours in this month is {this.hours}");
+            this.hours = 0;
+        }
+    }
+    class Student : Person
+    {
+        public Student(Names name)
+        {
+            this.id = 1;
+            this.name = name;
+        }
+        public float coeff { get; set; } = 0.5f;
+        public override void CalculateSalary(int hours)
+        {
+            float salary = 0;
+            while (hours > defaulHourCount)
+            {
+                hours--;
+                salary += salaryPerHour * coeff * 1;
+            }
+            while (hours > 0)
+            {
+                hours--;
+                salary += salaryPerHour * 1 * 1;
+            }
+            Random rand = new Random();
+            this.money += Convert.ToDouble(salary) + rand.Next(-700,701);
+            Console.WriteLine($"This is {this.name}, he is Student and his salary is {salary}, his hours in this month is {this.hours}");
+            this.hours = 0;
+        }
+    }
+
+    class Staff : Person
+    {
+        public Staff(Names name)
+        {
+            this.id = 2;
+            this.name = name;
+        }
+        public float coeff { get; set; } = 1.2f;
+        public override void CalculateSalary(int hours)
+        {
+            float salary = 0;
+            while (hours > defaulHourCount)
+            {
+                hours--;
+                salary += salaryPerHour * coeff * 1;
+            }
+            while (hours > 0)
+            {
+                hours--;
+                salary += salaryPerHour * 2 * 1;
+            }
+            this.money += Convert.ToDouble(salary);            
+            Console.WriteLine($"This is {this.name}, he is Staff and his salary is {salary}, his hours in this month is {this.hours}");
+            this.hours = 0;
+        }
+    }
+    internal class SecondTask
+    {
+        static List<Person> persons = new List<Person>();
+        static void Main()
+        {
+            Random rand = new Random();
+            Array enumValues = Enum.GetValues(typeof(Names));
+            for(int i = 0; i < 3 ; i++)
+            {
+                Professor professor = new Professor((Names)enumValues.GetValue(rand.Next(enumValues.Length)));
+                persons.Add(professor);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Student student = new Student((Names)enumValues.GetValue(rand.Next(enumValues.Length)));
+                persons.Add(student);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Staff staff = new Staff((Names)enumValues.GetValue(rand.Next(enumValues.Length)));
+                persons.Add(staff);
+            }
+            DoYearCycle();
+        }
+
+        static void DoYearCycle()
+        {
+            Random rnd = new Random();            
+            for (int i = 0; i< 12; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    foreach (Person person in persons)
+                    {
+                        switch (person.id)
+                        {
+                            case 0:
+                                person.hours += (8 + rnd.Next(-1, 4));
+                                break;
+                            case 1:
+                                person.hours += (8 + rnd.Next(-1, 4));
+                                break;
+                            case 2:
+                                person.hours += (8 + rnd.Next(-1, 4));
+                                break;
+                        }
+                    }
+                }
+                foreach (Person person in persons)
+                {
+                    person.CalculateSalary(person.hours);
+                }
+                Console.WriteLine("Press Enter to start next month");
+                Console.ReadLine();
+            }
+            foreach(Person person in persons)
+            {
+                Console.WriteLine($"Here is total salary of {person.name} - {person.money}");
+            }
+            Console.WriteLine("Year completed");
+        }
+    }
+}
